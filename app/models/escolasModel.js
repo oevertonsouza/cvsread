270ef7048217e429e47940c04b8ecdc587fb7e74
Everton de Souza <oevertonsouza@gmail.com>
2018-01-24 13:55:47 -0200

    Incluido mais campos no Model Escolas

--- app/models/escolasModel.js
+++ app/models/escolasModel.js
@@ -2,12 +2,49 @@ module.exports = function(){
 
   //Pegar todos os Escolas
   this.getEscolas = function(connection, callback){
-    connection.query('select * from ESCOLAS;', callback);
+    connection.query(`select
+                        ES.ID,
+                        ES.NOME,
+                        ES.DDD,
+                        ES.TELEFONE1,
+                        ES.TELEFONE2,
+                        ES.EMAIL,
+                        E.ENDERECO,
+                        E.NUMERO,
+                        E.COMPLEMENTO,
+                        B.DESCRICAO as BAIRRO,
+                        D.DESCRICAO as DISTRITO
+                      from ESCOLAS ES
+                        inner join ENDERECOS E on (ES.ENDERECO_ID = E.ID)
+                        inner join BAIRROS B on (E.BAIRRO_ID = B.ID)
+                        inner join DISTRITOS D on (E.DISTRITO_ID = D.ID)
+                      ;`, callback);
   };
 
   //Pegar Escolas por ID
   this.getEscolaById = function(id, connection, callback){
-    connection.query(`select * from ESCOLAS where ID = ${id};`, callback);
+    console.log(id);
+    connection.query(`select
+                        ES.ID,
+                        ES.NOME,
+                        ES.DDD,
+                        ES.TELEFONE1,
+                        ES.TELEFONE2,
+                        ES.EMAIL,
+                        E.ENDERECO,
+                        E.NUMERO,
+                        E.COMPLEMENTO,
+                        B.DESCRICAO as BAIRRO,
+                        D.DESCRICAO as DISTRITO,
+                        C.DESCRICAO  as CIDADE,
+                        EST.DESCRICAO  as ESTADO
+                      from ESCOLAS ES
+                        inner join ENDERECOS E on (ES.ENDERECO_ID = E.ID)
+                        inner join BAIRROS B on (E.BAIRRO_ID = B.ID)
+                        inner join DISTRITOS D on (B.DISTRITO_ID = D.ID)
+                        inner join CIDADES C on (D.CIDADE_ID = C.ID)
+                        inner join ESTADOS EST on (C.ESTADO_ID = EST.ID)
+                      where ES.ID = ${id};`, callback);
   };
 
   //Pegar Escola por ID
