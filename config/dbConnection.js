f59ce92e22e277452d15b53618826a3c6d97c4a1
root <root@localhost.localdomain>
2018-02-15 02:23:15 -0200

    Alter connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,11 +2,18 @@ var mysql = require('mysql');
 
 var connMySql = function(){
   return mysql.createConnection({
+    host: 'p2d0untihotgr5f6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'k70d3etlksurt4rt',
+    password : 'ja7q3p0s1uvj464u',
+    //password : '',
+    database : 'c0nuzcl01ayjt73k'
+    /*
     host: '127.0.0.1',
     user: 'root',
     password : 'Pms@2017',
     //password : '',
     database : 'redb'
+    */
   });
 };
 
