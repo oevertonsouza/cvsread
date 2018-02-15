eb5379d2c41af97787b73012d5c97d5d49f23b13
root <root@localhost.localdomain>
2018-02-15 03:12:10 -0200

    Connection

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var app = require('./config/server');
-app.listen(process.env.PORT || 5000);
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
 /*
 app.listen(4200, function(){
   console.log("Rodando com Express!");
