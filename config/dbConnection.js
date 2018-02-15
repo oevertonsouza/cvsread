dd83da123b0d2e80a058a99ec9cf7809aef1afc1
root <root@localhost.localdomain>
2018-02-15 03:25:57 -0200

    Connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 var connMySql = function(){
   return mysql.createConnection({
-    host: "p2d0untihotgr5f6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "k70d3etlksurt4rt",
-    password: "ja7q3p0s1uvj464u",
-    database : 'c0nuzcl01ayjt73k'
+    host: "",
+    user: "",
+    password: "",
+    database : ''
   });
 };
 
