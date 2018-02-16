c810bac381555a4f767224220b26770ebed702a8
Everton de Souza Lima <oevertonsouza@gmail.com>
2018-02-15 23:56:22 -0200

    Connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 var connMySql = function(){
   return mysql.createConnection({
-    host: "",
-    user: "",
-    password: "",
-    database : ""
+    host: "mysql427.umbler.com",
+    user: "moleria",
+    password: "moleiraindie",
+    database : "redb"
   });
 };
 
