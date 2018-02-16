efb5dfa9cc55054df171f0de8a69bfff53f4f1d5
Everton de Souza <oevertonsouza@gmail.com>
2018-02-16 18:46:57 -0200

    Variaveis de ambiente de conexão

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,16 +2,10 @@ var mysql = require('mysql');
 
 var connMySql = function(){
   return mysql.createConnection({
-    host: "mysql427.umbler.com",
-    user: "moleria",
-    password: "moleiraindie",
-    database : "redb"
-    /*
-      host: "127.0.0.1",
-      user: "root",
-      password: "",
-      database : "redb"
-    */
+    host: process.env.HOST_DB,
+    user: process.env.USER_DB,
+    password: process.env.PASSWORD_DB,
+    database : process.env.DATABASE_DB
   });
 };
 
